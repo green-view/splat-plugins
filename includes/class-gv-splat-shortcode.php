01271328312680ab5acd1661a5cfde60fda9d4a0
Indra Gunanda <indra.gunanda@gmail.com>
2024-10-20 14:11:39 +0000

    feat : add style

--- includes/class-gv-splat-shortcode.php
+++ includes/class-gv-splat-shortcode.php
@@ -2,49 +2,76 @@
 
 class Gv_Splat_Shortcode {
 
-	/**
-	 * Initialize the shortcode
-	 */
-	public static function init() {
-		add_shortcode( 'splat_shortcode', array( __CLASS__, 'render_splat_shortcode' ) );
-	}
+    /**
+     * Initialize the shortcode
+     */
+    public static function init() {
+        add_shortcode( 'splat_shortcode', array( __CLASS__, 'render_splat_shortcode' ) );
+    }
 
-	/**
-	 * Render the Splat shortcode
-	 *
-	 * @param array $atts Shortcode attributes.
-	 *
-	 * @return string HTML content or error message.
-	 */
-	public static function render_splat_shortcode( $atts ) {
-		// Get the Splat ID from the shortcode attributes
-		$atts = shortcode_atts( array(
-			'id' => 0, // Default to 0 if not provided
-		), $atts, 'splat_shortcode' );
+    /**
+     * Render the Splat shortcode
+     *
+     * @param array $atts Shortcode attributes.
+     *
+     * @return string HTML content or error message.
+     */
+    public static function render_splat_shortcode( $atts ) {
+        // Set default attributes
+        $atts = shortcode_atts( array(
+            'id'            => 0,          // Default ID
+            'width'         => '100%',     // Default width
+            'height'        => '100dvh',   // Default height
+            'border'        => 'none',     // Default border
+            'border_radius' => '',         // Default border-radius
+            'style'         => '',         // Custom style
+        ), $atts, 'splat_shortcode' );
 
-		$splat_id = intval( $atts['id'] );
+        $splat_id = intval( $atts['id'] );
 
-		// If no ID is provided, return an error message
-		if ( ! $splat_id ) {
-			return '<div class="notice notice-error">No valid Splat ID provided.</div>';
-		}
+        // If no ID is provided, return an error message
+        if ( ! $splat_id ) {
+            return '<div class="notice notice-error">No valid Splat ID provided.</div>';
+        }
 
-		// Fetch the Splat data
-		$splat_data = Gv_Splat_HTTP::get_splat( $splat_id );
+        // Fetch the Splat data
+        $splat_data = Gv_Splat_HTTP::get_splat( $splat_id );
 
-		// Check if data retrieval was successful
-		if ( ! isset( $splat_data['success'] ) || ! $splat_data['success'] ) {
-			return '<div class="notice notice-error">Error fetching Splat data.</div>';
-		}
+        // Check if data retrieval was successful
+        if ( ! isset( $splat_data['success'] ) || ! $splat_data['success'] ) {
+            return '<div class="notice notice-error">Error fetching Splat data.</div>';
+        }
 
-		// Generate the HTML for the Splat
-		$base = "https://staging.green-view.nl/s";
-		// If there's a thumbnail, display it
-		$output = "";
-		if ( ! empty( $splat_data['responseObject']['slug'] ) ) {
-			$output .= '<iframe src="' . esc_url( $base . "/" . $splat_data['responseObject']['slug'] ) . '" title="' . esc_url( $splat_data['responseObject']['title'] ) . '" style="width:100%; height:100dvh; border:none;"></iframe>';
-		}
+        // Generate the HTML for the Splat
+        $base = "https://staging.green-view.nl/s";
+        $output = "";
 
-		return $output;
-	}
+        if ( ! empty( $splat_data['responseObject']['slug'] ) ) {
+            // Build the style string
+            $style = '';
+
+            // If 'style' attribute is provided, use it directly
+            if ( ! empty( $atts['style'] ) ) {
+                $style = $atts['style'];
+            } else {
+                // Build style from individual attributes
+                $style_attributes = array(
+                    'width'  => esc_attr( $atts['width'] ),
+                    'height' => esc_attr( $atts['height'] ),
+                    'border' => esc_attr( $atts['border'] ),
+                );
+                if ( ! empty( $atts['border_radius'] ) ) {
+                    $style_attributes['border-radius'] = esc_attr( $atts['border_radius'] );
+                }
+                // Convert style attributes to a string
+                foreach ( $style_attributes as $key => $value ) {
+                    $style .= $key . ':' . $value . ';';
+                }
+            }
+
+            $output .= '<iframe src="' . esc_url( $base . "/" . $splat_data['responseObject']['slug'] ) . '" title="' . esc_attr( $splat_data['responseObject']['title'] ) . '" style="' . esc_attr( $style ) . '"></iframe>';
+        }
+
+        return $output;
+    }
 }
